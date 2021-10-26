958148f10766bdb41848248bbc0e25488b9c9f94
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-10-26 10:54:37 +0300

    Solution of problem 1. Rhombus of Stars

---
+++ 01_first_steps_in_oop_lab/01_rhombus_of_stars.py
@@ -0,0 +1,18 @@
+def print_rhombus():
+
+    n = int(input())
+
+    def print_line(spaces_count, stars_count):
+        print(" " * spaces_count, end="")
+        print(' '.join("*" * stars_count))
+
+    for i in range(1, n + 1):
+        spaces_count = n - i
+        stars_count = i
+        print_line(spaces_count, stars_count)
+    for i in range(n - 1, -1, -1):
+        spaces_count = n - i
+        stars_count = i
+        print_line(spaces_count, stars_count)
+
+print_rhombus()
