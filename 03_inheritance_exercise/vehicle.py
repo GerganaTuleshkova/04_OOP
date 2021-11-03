3e0daa024ae66d6b906c7ba4a998680a64c7483a
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-03 09:16:27 +0200

    Exercise "Inheritance"

---
+++ 03_inheritance_exercise/vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+        self.fuel = fuel
+        self.horse_power = horse_power
+
+    def drive(self, kilometers):
+        fuel_needed = kilometers * self.fuel_consumption
+        if self.fuel < fuel_needed:
+            return
+        self.fuel -= fuel_needed
