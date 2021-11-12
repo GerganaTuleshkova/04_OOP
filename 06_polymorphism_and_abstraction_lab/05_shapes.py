1d3a9729ac5d1c68a96246169bac72461b53a529
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-12 14:16:02 +0200

    Lab "Polymorphism and abstraction"

---
+++ 06_polymorphism_and_abstraction_lab/05_shapes.py
@@ -0,0 +1,43 @@
+import math
+from abc import abstractmethod, ABC
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return self.__radius ** 2 * math.pi
+
+    def calculate_perimeter(self):
+        return self.__radius * 2 * math.pi
+
+
+class Rectangle:
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__width * self.__height
+
+    def calculate_perimeter(self):
+        return 2 * (self.__height + self.__width)
+
+
+circle = Circle(5)
+print(circle.calculate_area())
+print(circle.calculate_perimeter())
+rectangle = Rectangle(10, 20)
+print(rectangle.calculate_area())
+print(rectangle.calculate_perimeter())
