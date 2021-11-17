0cf344e1c238bf0f480aeea77f34874d85cc01dd
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-17 10:46:48 +0200

    Exercise SOLID

---
+++ 07_SOLID_exercise/exercise-resources/emails.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta, abstractmethod, ABC
+
+
+class IEmail(ABC):
+    @abstractmethod
+    def set_sender(self, sender):
+        pass
+
+    @abstractmethod
+    def set_receiver(self, receiver):
+        pass
+
+
+class Email(IEmail):
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.__sender = None
+        self.__receiver = None
+        self.__content = None
+
+    def set_sender(self, sender):
+        if self.protocol == 'IM':
+            self.__sender = ''.join(["I'm ", sender])
+        else:
+            self.__sender = sender
+
+    def set_receiver(self, receiver):
+        if self.protocol == 'IM':
+            self.__receiver = ''.join(["I'm ", receiver])
+        else:
+            self.__receiver = receiver
+
+    def set_content(self, content):
+        self.__content = str(content)
+
+    def __repr__(self):
+        return f"Sender: {self.__sender}\nReceiver: {self.__receiver}\nContent:\n{self.__content}"
+
+
+class IContent(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class MyContent(IContent):
+    def __init__(self, content):
+        self.content = content
+
+    def __str__(self):
+        return f"{''.join(['<MyML>', self.content, '</MyML>'])}"
+
+
+email = Email('IM')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = MyContent('Hello, there!')
+email.set_content(content)
+print(email)
