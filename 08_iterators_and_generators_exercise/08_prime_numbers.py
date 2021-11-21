ee2a2c0f7b30ab73076e015aec1f798f246f867d
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-21 15:22:32 +0200

    Exercise Iterators and Generators - v.2

--- 08_iterators_and_generators_exercise/08_prime_numbers.py
+++ 08_iterators_and_generators_exercise/08_prime_numbers.py
@@ -1,16 +1,21 @@
+def is_prime(n):
+    is_prime_number = True
+    if n <= 1:
+        return False
+    for delimiter in range(2, n):
+        if n % delimiter == 0:
+            is_prime_number = False
+            break
+    return is_prime_number
+
+
 def get_primes(given_list):
     for n in given_list:
-        is_prime = True
-        if n < 1:
-            is_prime = False
-            break
-        for delimiter in range(2, n):
-            if n % delimiter == 0:
-                is_prime = False
-                break
-        if is_prime:
+        if is_prime(n):
             yield n
 
 
 print(list(get_primes([-2, 0, 0, 1, 1, 0])))
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
 
