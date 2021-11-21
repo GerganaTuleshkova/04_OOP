a28e9e528df2044d22caed4a9ce5134f89bd5d61
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-21 14:19:26 +0200

    Exercise Iterators and Generators - v.2

--- 08_iterators_and_generators_exercise/05_take_halves.py
+++ 08_iterators_and_generators_exercise/05_take_halves.py
@@ -11,14 +11,17 @@ def solution():
             yield i / 2
 
     def take(n, seq):
-        count = 1
+        #count = 1
         result = []
-        while count <= n:
-            for i in seq:
-                result.append(i)
-                count += 1
-                if count >= n:
-                    break
+        # while count <= n:
+        #     for i in seq:
+        #         result.append(i)
+        #         count += 1
+        #         if count >= n:
+        #             break
+        for _ in range(n):
+            result.append(next(seq))
+
         return result
 
     return (take, halves, integers)
@@ -28,29 +31,6 @@ take = solution()[0]
 halves = solution()[1]
 print(take(0, halves()))
 
-
-
-# def integers():
-#     number = 1
-#     while True:
-#         yield number
-#         number += 1
-#
-#
-# def halves():
-#     for i in integers():
-#         yield i / 2
-#
-#
-# def take(n, seq):
-#     count = 1
-#     result = []
-#     while count <= n:
-#         for i in halves():
-#             result.append(i)
-#             count += 1
-#             if count >= n:
-#                 break
-#     return result
-#
-# print(take(5, halves()))
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
