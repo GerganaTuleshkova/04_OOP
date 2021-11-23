9309d728a959d21a142bb1d5cc47d66a643a4230
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2021-11-23 15:34:28 +0200

    Lab - Decorators

---
+++ 09_decorators_lab/03_even_numbers.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+
+def even_numbers(function):
+    @wraps(function)
+    def wrapper(numbers):
+        result = function(numbers)
+        return [n for n in result if n % 2 == 0]
+    return wrapper
+
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
